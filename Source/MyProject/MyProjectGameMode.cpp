b799480b36ae9e6d5f3811f6500f043394ea8b8b
Anup Vasudevan <rtre84@users.noreply.github.com>
2018-01-24 05:44:35 -0500

    Initial commit

---
+++ Source/MyProject/MyProjectGameMode.cpp
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "MyProjectGameMode.h"
+#include "MyProjectPlayerController.h"
+#include "MyProjectCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMyProjectGameMode::AMyProjectGameMode()
+{
+	// use our custom PlayerController class
+	PlayerControllerClass = AMyProjectPlayerController::StaticClass();
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
