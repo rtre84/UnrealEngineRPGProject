0422c74e77f826c73a9316cde9c5f1075a0cae7c
Anup Vasudevan <rtre84@users.noreply.github.com>
2018-01-24 06:19:04 -0500

    Added ...ProjectUserSettings.ini

---
+++ cmake-build-debug/CMakeFiles/MyProject.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProject.init.gen.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProject.init.gen.cpp.o"
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectCharacter.gen.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectCharacter.gen.cpp.o"
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectGameMode.gen.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectGameMode.gen.cpp.o"
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectPlayerController.gen.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectPlayerController.gen.cpp.o"
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Source/MyProject/MyProject.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Source/MyProject/MyProject.cpp.o"
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Source/MyProject/MyProjectCharacter.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Source/MyProject/MyProjectCharacter.cpp.o"
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Source/MyProject/MyProjectGameMode.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Source/MyProject/MyProjectGameMode.cpp.o"
+  "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/Source/MyProject/MyProjectPlayerController.cpp" "/Users/anupvasudevan/Documents/Unreal Projects/MyProject/cmake-build-debug/CMakeFiles/MyProject.dir/Source/MyProject/MyProjectPlayerController.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProject.init.gen.cpp.o"
+  "CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectCharacter.gen.cpp.o"
+  "CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectGameMode.gen.cpp.o"
+  "CMakeFiles/MyProject.dir/Intermediate/Build/Mac/UE4Editor/Inc/MyProject/MyProjectPlayerController.gen.cpp.o"
+  "CMakeFiles/MyProject.dir/Source/MyProject/MyProject.cpp.o"
+  "CMakeFiles/MyProject.dir/Source/MyProject/MyProjectCharacter.cpp.o"
+  "CMakeFiles/MyProject.dir/Source/MyProject/MyProjectGameMode.cpp.o"
+  "CMakeFiles/MyProject.dir/Source/MyProject/MyProjectPlayerController.cpp.o"
+  "MyProject.pdb"
+  "MyProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
